39b8c09d2ea819faa37eb11b3d246155f2570678
leng-chu <jacklearn@DESKTOP-HI558GB.localdomain>
2022-11-28 19:19:58 +0800

    enable if

--- LEARN/main.cc
+++ LEARN/main.cc
@@ -6,7 +6,7 @@
 /*   By: leng-chu <-chu@student.42kl.edu.my>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/14 16:31:37 by leng-chu          #+#    #+#             */
-/*   Updated: 2022/11/22 11:22:00 by leng-chu         ###   ########.fr       */
+/*   Updated: 2022/11/28 19:06:42 by leng-chu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 #include <iterator>
 #include <unordered_map>
 #include <memory> // for unique make
+#include <type_traits> // for enable_if
 
 using std::cout;
 using std::endl;
@@ -26,11 +27,51 @@ using std::iterator_traits;
 using std::list;
 using std::unordered_map;
 using std::string;
+using std::enable_if;
+using std::ostream;
 
-template <class _iter>
-typename enable_if< _Is_iterator<_Iter>::value, void>::type
-assign(__Iter _First, _Iter _Last)
-{...}
+// non-type template parameter
 
-void assign(size_type _Count, const value_type & Val)
-{...}
+template <int N>
+class A
+{
+	public:
+	int v{N};
+	template<int J> friend ostream& operator<< (ostream &,A<J>&);
+};
+
+template <int N = 10>
+class B
+{
+	int v{N};
+};
+
+template <int = 10>
+class C
+{
+	int v{0};
+};
+
+template <int>
+class D
+{
+	int v{0};
+};
+
+template <int, int>
+class E
+{
+	int v{0};
+};
+
+template<int J> ostream& operator<< (ostream & o,A<J> & a)
+{
+	o << "A: " << a.v;
+	return (o);
+}
+int main()
+{
+	A<19> a{};
+	cout << a << endl;
+	return (0);
+}
